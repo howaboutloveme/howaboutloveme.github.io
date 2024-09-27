d5e74bf3b454c11c9eb61c5c87e1e3f360efcb6b
howaboutloveme <102989110+howaboutloveme@users.noreply.github.com>
2024-09-27 13:42:33 +0800

    更新 js

---
+++ js/91.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    const leftText = document.getElementById('leftText');
+    const rightText = document.getElementById('rightText');
+    
+    leftText.style.transform = 'translateX(0)';
+    rightText.style.transform = 'translateX(0)';
+};
